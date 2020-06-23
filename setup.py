26027fcd0625e4cbe3f29aced7516429e351a1eb
Vimal Daga <vimal.linuworld@gmail.com>
2020-06-23 16:33:09 +0530

    tox example

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='elegantcasing',
+    name='lwcasing',
     version='1.0.1',
     description='Change your text to title-case',
-    author='Brandon Blair',
-    author_email='cbrandon.blair@gmail.com',
-    url='https://github.com/BrandonBlair/elegantframeworks',
+    author='Vimal Daga',
+    author_email='vimal.linuxworld@gmail.com',
+    url='https://github.com/vimaldaga13/lwframeworks',
     packages=find_packages()
 )
